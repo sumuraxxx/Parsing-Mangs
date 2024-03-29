85d6e8858174ab801e362c8b9d1525afe40a464b
sumuraxxx <143743357+sumuraxxx@users.noreply.github.com>
2024-03-29 18:06:32 +0400

    Init

---
+++ get_id_hentay_pages.py
@@ -0,0 +1,95 @@
+import aiohttp
+import asyncio
+import fake_useragent
+from bs4 import BeautifulSoup
+import re
+
+user = fake_useragent.UserAgent().random
+
+header = {"user-agent": user}
+url = "https://ahri-gallery.com/index.php?route=comic/list&t=1709222400&category_id=6"
+
+
+async def get_manges():
+    comic_info = {}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                html = await response.text()
+                soup = BeautifulSoup(html, "lxml")
+
+                images = soup.find_all("div", class_="image-inner")
+                links = soup.find_all("a", class_="apo")
+                titles = soup.find_all("div", class_="image-info")
+
+                for title, link, img_div in zip(titles, links, images):
+                    name = title.find("h5").find("span").text
+                    href = link.get("href")
+                    img_tag = img_div.find("img")
+                    if href[0] == ".":
+                        href = None
+
+                    if img_tag:
+                        image_url = img_tag.get("src")
+                        if image_url:
+                            id = image_url[44:51]
+                            comic_info[name] = {
+                                "href": href,
+                                "image_url": image_url,
+                                "id": id
+                            }
+
+            else:
+                print("Ошибка при получении страницы:", response.status)
+
+    return comic_info
+
+
+async def get_link_for_download_photo(session, href):
+    links = []
+    urls = []
+    async with session.get(f'https://ahri-gallery.com/{href}') as res:
+        if res.status == 200:
+            soup = BeautifulSoup(await res.text(), "lxml")
+
+            div_tag = soup.find("div", class_="btn-group action_btn")
+            if div_tag:
+                link = div_tag.find("a").get("href")
+                if link:
+                    async with session.get(f'https://ahri-gallery.com/{link}', headers=header) as res2:
+                        urls.append(f"https://ahri-gallery.com/{link}")
+                        soup = BeautifulSoup(await res2.text(), "lxml")
+
+                        text = str(soup.find("script", language="javascript"))
+
+                        text = text.replace('<script language="javascript">', '')
+                        match = re.findall(r'"(.*?)"', text)[0]
+                        if match:
+                            links.append(match)
+        return links, urls
+
+
+async def main():
+    manges = await get_manges()
+    with open("../manges.txt", "w", encoding="UTF-8") as file:
+        for key, value in manges.items():
+            file.write(f"{key}: {value}\n")
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [get_link_for_download_photo(session, info["href"]) for name, info in manges.items() if info["href"]]
+        results = await asyncio.gather(*tasks)
+        links, urls = zip(*results)
+
+        with open("../links1.txt", "w") as file:
+            for link_list in links:
+                for i in link_list:
+                    file.write(f"{i}\n")
+
+        with open("../urls1.txt", "w") as file:
+            for url_list in urls:
+                for i in url_list:
+                    file.write(f"{i}\n")
+
+
+asyncio.run(main())
---
+++ get_photo.py
@@ -0,0 +1,96 @@
+import aiohttp
+import asyncio
+import re
+from bs4 import BeautifulSoup
+from PIL import Image
+import fake_useragent
+import os
+
+user = fake_useragent.UserAgent().random
+
+headers = {
+    'sec-ch-ua': '"Not A(Brand";v="99", "Opera GX";v="107", "Chromium";v="121"',
+    'Referer': 'https://ahri-gallery.com/',
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': user,
+    'sec-ch-ua-platform': '"Windows"',
+}
+
+
+async def get_count_pages(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            html = await response.text()
+            soup = BeautifulSoup(html, "lxml")
+            pages = soup.find("script", language="javascript")
+            matches = list(re.findall(r'\[(.*?)\]', str(pages), re.DOTALL))
+            dicts_list = matches[1].split('},{')
+            dicts_list[0] = dicts_list[0] + '}'
+            dicts_list[-1] = '{' + dicts_list[-1]
+            count_photo = len(dicts_list) + 1
+            return count_photo
+
+
+async def download_photo(link, url, task_num):
+    try:
+        match = re.search(r'/(\d+)/(\d+)/$', link)
+        id_1 = int(match.group(1))
+        id_2 = int(match.group(2))
+
+        count_photo = await get_count_pages(url)
+
+        async with aiohttp.ClientSession() as session:
+            for i in range(1, count_photo):
+                urls = [f'https:{link}{i}.jpg', f'https:{link}{i}.webp']
+                for img_url in urls:
+                    response = await session.get(img_url, headers=headers)
+                    data = await response.read()
+                    if response.status == 200:
+                        if img_url.endswith('.webp'):
+                            img_dir = f"temp/{id_1}_{task_num}"
+                            if not os.path.exists(img_dir):
+                                os.makedirs(img_dir)
+
+                            with open(f"{img_dir}/haha{i}.webp", "wb") as file:
+                                file.write(data)
+                            with Image.open(f"{img_dir}/haha{i}.webp") as img:
+                                img_dir = f"hentays/{id_2}"
+                                if not os.path.exists(img_dir):
+                                    os.makedirs(img_dir)
+                                img = img.convert('RGB')
+                                img.save(f'hentays/{id_2}/haha{i}.jpg', 'jpeg')
+                        else:
+                            img_dir = f"hentays/{id_2}"
+                            if not os.path.exists(img_dir):
+                                os.makedirs(img_dir)
+
+                            with open(f"{img_dir}/haha{i}.jpg", "wb") as file:
+                                file.write(data)
+    except:
+        print("error. проблемы с сервером")
+
+
+async def main():
+    urls, links = get_links_and_urls_and_ids(int(input()))
+
+    tasks = [download_photo(link, url, task_num) for task_num, (link, url) in enumerate(zip(links, urls), 1)]
+
+    await asyncio.gather(*tasks)
+
+
+def get_links_and_urls_and_ids(number_file):
+    links = []
+    with open(f"links{number_file}.txt", "r", encoding="UTF-8") as file:
+        for line in file:
+            links.append(line.strip())
+
+    urls = []
+    with open(f"urls{number_file}.txt", "r", encoding="UTF-8") as file:
+        for line in file:
+            urls.append(line.strip())
+
+    return urls, links
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
